7c895778dee8cc7d521ddeb24658fd852dba59fa
HamzaHafeez <HamzaHafeez@HAMZA>
2024-12-10 11:53:45 +0500

    Make User Model, Edit Home Controller and index file to show users

--- CHATAPP/Controllers/HomeController.cs
+++ CHATAPP/Controllers/HomeController.cs
@@ -1,23 +1,70 @@
 using System.Diagnostics;
 using CHATAPP.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace CHATAPP.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private string connectionString = "Data Source=Hamza;Initial Catalog=CHATAPP;Integrated Security=True;TrustServerCertificate=True;";
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
-            return View();
+            List<Users> users = GetUsers();
+            return View(users);
         }
+        [Obsolete]
+        public List<Users> GetUsers()
+        {
+            List<Users> userslist = new List<Users>();
+            string currentuser = User.Identity.Name;
+            string query = "SELECT Name, UserName FROM tbl_users WHERE UserName != @currentuser";
 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Add the parameter to avoid SQL injection
+                        command.Parameters.AddWithValue("@currentuser", currentuser);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Users user = new Users
+                                {
+                                    Name = reader["Name"].ToString(),
+                                    Username = reader["UserName"].ToString()
+                                };
+                                userslist.Add(user);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+
+            return userslist;
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Access");
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ CHATAPP/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace CHATAPP.Models
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+    }
+}
